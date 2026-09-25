a045810d67949806800b6b546f9195b9cc96b5b5
kenny1-ulol <lancepalen9@gmail.com>
2026-09-25 21:41:48 +0800

    Create TryThisAtHome1.java

---
+++ TryThisAtHome1.java
@@ -0,0 +1,19 @@
+public class TryThisAtHome1 {
+    public static void main(String[] args) {
+
+        // body of the main
+        String firstName = "Lance Cedrick";
+        String lastName = "Palen";
+        int birthYear = 2008;
+
+        //current year and current age varibles
+        int currentYear = 2026;
+        int currentAge = currentYear - birthYear;
+
+        //output print statements
+        System.out.println("Enter Firstname: " + firstName);
+        System.out.println("Enter Lastname: " + lastName);
+        System.out.println("Enter Birthyear: " + birthYear);
+        System.out.println("Hello " + firstName + lastName + "! Your current age is " + currentAge + ".");
+    }
+}
